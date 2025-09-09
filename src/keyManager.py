985f075bdffe19fd6a1a4d82903eaaf086db77c4
Caleb Singleton <cmsingle12@gmail.com>
2025-09-09 00:12:33 -0400

    Corrected: Added generate_key and load_key functions

--- src/keyManager.py
+++ src/keyManager.py
@@ -1,5 +1,15 @@
+import os.path
+from cryptography.fernet import Fernet
+
 def generate_key():
-    return
+    key = Fernet.generate_key()
+    with open ("secret.key", "wb") as key_file:
+        key_file.write(key)
 
 def load_key():
-    return
+    if os.path.exists("secret.key"):
+        with open("secret.key","rb") as key_file:
+            read_key = key_file.read()
+    else:
+        print("There are no previously loaded keys. ")
+    return read_key
